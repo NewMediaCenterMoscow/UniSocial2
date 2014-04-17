d767588a124308982d52dd78cc7be03c2ec7d9db
Alexey Romanov <jgc128@outlook.com>
2014-04-17 19:05:19 +0400

    SqlInsertFormatter is working!

--- Worker/Common/Formatters/SqlInsertFormatter.cs
+++ Worker/Common/Formatters/SqlInsertFormatter.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Worker.Common.Formatters
 {
-	public class SqlInsertFormatter : CSVStreamFormatter
+	public class SqlInsertFormatter : DataReaderFormatter
 	{
 		string tableName;
 		public SqlInsertFormatter(string TableName)
@@ -17,18 +17,79 @@ namespace Worker.Common.Formatters
 
 		protected override object GetResult()
 		{
-			MemoryStream stream = (MemoryStream)base.GetResult();
-			string csvString = Encoding.UTF8.GetString(stream.ToArray());
-			var rows = csvString.Split('\n');
+			//MemoryStream stream = (MemoryStream)base.GetResult();
+			//string csvString = Encoding.UTF8.GetString(stream.ToArray());
+			//var rows = csvString.Split('\n');
 
+			//var beforeSql = "INSERT INTO " + tableName + " VALUES (";
+			//var afterSql = ")";
+			//var result = 
+			//	from row in rows
+			//	select beforeSql + row + afterSql;
+
+
+			//return result.ToList();
+
+
+
+			var reader = (UniSocialObjectsDataReader)base.GetResult();
+			
 			var beforeSql = "INSERT INTO " + tableName + " VALUES (";
 			var afterSql = ")";
-			var result = 
-				from row in rows
-				select beforeSql + row + afterSql;
+			var result = new List<string>();
 
+			int fieldCount = reader.FieldCount;
+			int i;
+			var sql = new StringBuilder();
 
-			return result.ToList();
+			while (reader.Read())
+			{
+				sql.Clear().Append(beforeSql);
+				for (i = 0; i < fieldCount; i++)
+				{
+					var value = reader.GetValue(i);
+
+					if (value == null)
+					{
+						sql.Append("NULL,");
+					}
+					else
+					{
+						Type t = value.GetType();
+
+						if (value is string)
+						{
+							value = ((string)value).Replace("'", @"\'");
+							sql.Append("'").Append(value).Append("',");
+						}
+						else if (t.IsEnum)
+						{
+							sql.Append((int)value).Append(",");
+						}
+						else if (value is bool)
+						{
+							if ((bool)value)
+							{
+								sql.Append(1).Append(",");
+							}
+							else
+							{
+								sql.Append(0).Append(",");
+							}
+						}
+						else 
+						{
+							sql.Append(value).Append(",");
+						}
+					}
+				}
+				sql.Remove(sql.Length-1,1).Append(afterSql);
+
+				result.Add(sql.ToString());
+			}
+
+			return result;
+
 		}
 
 	}
